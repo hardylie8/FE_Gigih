b79202e4b95fe81dc13cdb1005ea59cd1adea5f8
hardyliyanto <hardyliyanto@gmail.com>
2023-08-28 00:13:52 +0700

    updated route

--- src/Router.jsx
+++ src/Router.jsx
@@ -30,7 +30,7 @@ export default function Router() {
           element: <VideoDetail />,
         },
         {
-          path: "/home",
+          path: "/",
           element: <Home />,
         },
         {},
